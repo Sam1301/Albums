f2a10cd8d8da19285090799d9d02cdd3e6ac73a1
Saumya Bhatnagar <saumya.bhatnagar.sb@gmail.com>
2017-03-24 18:42:11 +0530

    Scrollview works

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,14 +1,55 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, Image, View, StyleSheet } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
 
-const AlbumDetail = (props) => (
+const AlbumDetail = (props) => {
+const { title, artist, thumbnail_image, image } = props.album;
+const { thumbnailStyle,
+  headerContainerStyle,
+  thumbnailContainerStyle,
+  headerTextStyle,
+  imageStyle } = styles;
+console.log(thumbnail_image);
+return (
     <Card>
       <CardSection>
-        <Text>{props.album.title}</Text>
+        <View style={thumbnailContainerStyle}>
+          <Image source={{ uri: thumbnail_image }} style={thumbnailStyle} />
+        </View>
+        <View style={headerContainerStyle}>
+          <Text style={headerTextStyle}>{title}</Text>
+          <Text>{artist}</Text>
+        </View>
+      </CardSection>
+      <CardSection>
+        <Image source={{ uri: image }} style={imageStyle} />
       </CardSection>
     </Card>
   );
+};
 
+const styles = StyleSheet.create({
+  headerContainerStyle: {
+    justifyContent: 'space-around',
+    flexDirection: 'column'
+  },
+  headerTextStyle: {
+    fontSize: 18
+  },
+  thumbnailContainerStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 10,
+    marginRight: 10
+  },
+  thumbnailStyle: {
+    width: 50,
+    height: 50
+  },
+  imageStyle: {
+    height: 300,
+    flex: 1
+  }
+});
 export default AlbumDetail;
--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { ScrollView } from 'react-native';
 import axios from 'axios';
 import AlbumDetail from './AlbumDetail';
 
@@ -23,14 +23,15 @@ class AlbumList extends Component {
   }
 
   renderAlbums() {
+    console.log(this.state.albums);
     return this.state.albums.map(album => <AlbumDetail key={album.title} album={album} />);
   }
   render() {
     console.log(this.state);
     return (
-      <View>
+      <ScrollView>
         {this.renderAlbums()}
-      </View>
+      </ScrollView>
     );
   }
 }
